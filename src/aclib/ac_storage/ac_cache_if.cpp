2e70936c5f4e5901d23790c28399066d750714b3
Stefan Wallentowitz <stefan.wallentowitz@tum.de>
2011-06-11 12:29:23 +0200

    Import original source code (Version 2.1)

---
+++ src/aclib/ac_storage/ac_cache_if.cpp
@@ -0,0 +1,38 @@
+/**
+ * @file      ac_cache_if.cpp
+ * @author    The ArchC Team
+ *            http://www.archc.org/
+ *
+ *            Computer Systems Laboratory (LSC)
+ *            IC-UNICAMP
+ *            http://www.lsc.ic.unicamp.br/
+ *
+ * @version   1.0
+ * @date      Mon, 19 Jun 2006 15:33:19 -0300
+ *
+ * @brief     
+ *
+ * @attention Copyright (C) 2002-2006 --- The ArchC Team
+ * 
+ * This program is free software; you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation; either version 2 of the License, or 
+ * (at your option) any later version. 
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU General Public License for more details. 
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#include "ac_cache_if.H"
+
+void bind(ac_cache_if& previous, ac_cache_if& next) {
+  previous.bindToNext(next);
+  next.bindToPrevious(previous);
+}
